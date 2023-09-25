37e1c40d80c2f3c49ce9b3c1f3c9e820558bfe4d
Dragon <goldendragon0710@gmail.com>
2023-09-25 09:14:33 -0700

    update the source code due to building

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -8,18 +8,17 @@ import {
   TabsBody,
   Tab,
   TabPanel,
+  Typography,
 } from "@material-tailwind/react";
 import FilesTab from "@/widgets/tabs/files";
 import LinksTab from "@/widgets/tabs/links";
 import SitemapsTab from "@/widgets/tabs/sitemaps";
 import FAQsTabs from "@/widgets/tabs/FAQs";
-import ClipLoader from "react-spinners/ClipLoader";
-import { Typography } from "antd";
 import { TrashIcon } from "@heroicons/react/24/outline";
+import { ClipLoader } from "react-spinners";
 
 export function Home() {
   const [loading, setLoading] = useState(false);
-  const [prompt, setPrompt] = useState("");
   const [dataset, setDataset] = useState([]);
 
   useEffect(() => {
@@ -75,29 +74,11 @@ export function Home() {
       });
   };
 
-  const handlePromptChange = (e) => {
-    setPrompt(e.target.value);
-  };
-
-  const handlepromptsend = () => {
-    setLoading(true);
-    axios
-      .post(`${process.env.REACT_APP_BASED_URL}/prompt`, { prompt: prompt })
-      .then((res) => {
-        setLoading(false);
-        console.log(res.data);
-      })
-      .catch((err) => {
-        setLoading(false);
-        console.log(err);
-      });
-  };
-
   return (
     <>
-      <div className="relative flex h-screen content-center justify-center pb-32 pt-16">
-        <div className="max-w-8xl container relative mx-auto">
-          <div className="flex flex-col flex-wrap items-center bg-[#b0b7bb]">
+      <div className="relative flex content-center justify-center pb-16 pt-16">
+        <div className="max-w-8xl container relative mx-auto mb-10 ">
+          <div className="flex h-full flex-col items-center bg-[#b0b7bb]">
             <div className="ml-auto mr-auto mt-10 w-full px-4 py-5 text-center lg:w-8/12">
               <Tabs value="Upload">
                 <TabsHeader>
@@ -127,38 +108,30 @@ export function Home() {
               {dataset &&
                 dataset.map((item, idx) => {
                   return (
-                    <div key={idx} className="flex w-full justify-between">
+                    <div
+                      key={idx}
+                      className="flex w-full items-center justify-between border-[1px] border-solid p-2"
+                    >
                       <Typography>{item.name}</Typography>
                       <Button
                         variant="text"
                         onClick={() => handleDeleteDataset(item._id)}
                         className="flex h-8 w-8 items-center justify-center p-0"
                       >
-                        <TrashIcon className="w-6" color="black" />
+                        {loading ? (
+                          <ClipLoader
+                            color={"#000000"}
+                            loading={loading}
+                            size={13}
+                            cssOverride={{ height: "25px", width: "25px" }}
+                          />
+                        ) : (
+                          <TrashIcon className="w-6" color="black" />
+                        )}
                       </Button>
                     </div>
                   );
                 })}
-            </div>
-          </div>
-          <div className="my-5 w-full rounded bg-white p-4">
-            <Textarea label="Message" onChange={handlePromptChange} />
-            <div className="flex w-full justify-end">
-              <Button
-                className=" text-[16px] normal-case"
-                onClick={handlepromptsend}
-              >
-                {loading ? (
-                  <ClipLoader
-                    color={"#000000"}
-                    loading={loading}
-                    size={13}
-                    cssOverride={{ height: "25px", width: "25px" }}
-                  />
-                ) : (
-                  <Typography>Send</Typography>
-                )}
-              </Button>
             </div>
           </div>
         </div>
--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -4,8 +4,8 @@ import { HomeIcon } from "@heroicons/react/24/solid";
 export const routes = [
   {
     icon: HomeIcon,
-    name: "home",
-    path: "/home",
+    name: "Home",
+    path: "/",
     element: <Home />,
   },
 ];
