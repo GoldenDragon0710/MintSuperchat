4be99896dd573b3ea987ddc5d29e3c0bea86c2f9
goldendragon0710 <goldendragon0710@gmail.com>
2023-12-11 09:01:16 +0200

    fixed router management

--- frontend/src/pages/Layout.jsx
+++ frontend/src/pages/Layout.jsx
@@ -1,19 +1,20 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useSelector } from "react-redux";
 import { Navigate, Outlet } from "react-router-dom";
 
 export function Layout() {
   const auth = useSelector((state) => state.auth);
   const token = localStorage.getItem("token");
-  const [isView, setIsView] = useState(false);
 
-  useEffect(() => {
-    if (auth.user && auth.user.userType == process.env.isClient) {
-      setIsView(true);
-    }
-  }, []);
-
-  return <div>{token && isView ? <Outlet /> : <Navigate to="/login" />}</div>;
+  return (
+    <div>
+      {token ? (
+        auth.user && auth.user.userType == process.env.isClient && <Outlet />
+      ) : (
+        <Navigate to="/login" />
+      )}
+    </div>
+  );
 }
 
 export default Layout;
--- frontend/src/pages/adminLayout.jsx
+++ frontend/src/pages/adminLayout.jsx
@@ -1,20 +1,19 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useSelector } from "react-redux";
 import { Navigate, Outlet } from "react-router-dom";
 
 export function AdminLayout() {
   const auth = useSelector((state) => state.auth);
   const token = localStorage.getItem("token");
-  const [isView, setIsView] = useState(false);
-
-  useEffect(() => {
-    if (auth.user && auth.user.userType == process.env.isAdmin) {
-      setIsView(true);
-    }
-  }, []);
 
   return (
-    <div>{token && isView ? <Outlet /> : <Navigate to="/admin/login" />}</div>
+    <div>
+      {token ? (
+        auth.user && auth.user.userType == process.env.isAdmin && <Outlet />
+      ) : (
+        <Navigate to="/admin/login" />
+      )}
+    </div>
   );
 }
 
