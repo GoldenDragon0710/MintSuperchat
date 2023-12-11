e6932ff3734339d90aac3550ebb66bd512789b0c
goldendragon0710 <goldendragon0710@gmail.com>
2023-12-11 08:55:37 +0200

    update router management by user role

--- frontend/src/pages/Layout.jsx
+++ frontend/src/pages/Layout.jsx
@@ -1,20 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { Navigate, Outlet } from "react-router-dom";
 
 export function Layout() {
   const auth = useSelector((state) => state.auth);
   const token = localStorage.getItem("token");
+  const [isView, setIsView] = useState(false);
 
-  return (
-    <div>
-      {token ? (
-        auth.user && auth.user.userType == process.env.isClient && <Outlet />
-      ) : (
-        <Navigate to="/login" />
-      )}
-    </div>
-  );
+  useEffect(() => {
+    if (auth.user && auth.user.userType == process.env.isClient) {
+      setIsView(true);
+    }
+  }, []);
+
+  return <div>{token && isView ? <Outlet /> : <Navigate to="/login" />}</div>;
 }
 
 export default Layout;
